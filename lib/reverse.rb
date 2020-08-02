2831c1224ea3aad994129beee453220a0496c109
Saryn Mooney <smooney15@my.whitworth.edu>
2020-08-02 13:59:07 -0600

    added split method to reverse class

--- lib/reverse.rb
+++ lib/reverse.rb
@@ -1,17 +1,56 @@
 require './lib/dictionary'
 
 class Reverse
-  attr_reader :reverse_characters
+  attr_reader :reverse_characters, :message
 
-  def initialize
+  def initialize(message)
+    @message = message
     @reverse_characters = Dictionary.new.characters.invert
   end
 
-  def translate(message)
+  def translate
     result = message.map do |letter|
       @reverse_characters[letter]
     end
     result.join
+  end
+
+  def split
+    array = translate
+    number = (array.length / 3)
+    array.scan(/.{1,#{number}}/)
+  end
+
+  def top
+    out_top = []
+    out_top << split.fetch(0).scan(/../)
+    return out_top.flatten
+  end
+
+  def mid
+    out_mid = []
+    out_mid << split.fetch(1).scan(/../)
+    return out_mid.flatten
+  end
+
+  def bottom
+    out_bottom = []
+    out_bottom << split.fetch(2).scan(/../)
+    return out_bottom.flatten
+  end
+
+  def zip_characters_together
+    top.zip(mid, bottom)
+  end
+
+  def convert_to_english
+    zip_characters_together.map do |letter|
+      @english_alphabet[letter]
+    end
+  end
+
+  def english_result
+    result = convert_to_english
   end
 
 end
--- test/reverse_test.rb
+++ test/reverse_test.rb
@@ -9,14 +9,19 @@ require './lib/reverse'
 class ReverseTest < Minitest::Test
 
   def test_it_exists
-    reverse = Reverse.new
+    reverse = Reverse.new([["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]])
     assert_instance_of Reverse, reverse
   end
 
   def test_it_can_translate
-    reverse = Reverse.new
-    characters = [["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]]
-    assert_equal "hello", reverse.translate(characters)
+    reverse = Reverse.new([["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]])
+    assert_equal "hello", reverse.translate
+  end
+
+  def test_it_can_split_string_of_braille_characters
+    reverse = Reverse.new([["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]])
+    reverse.translate
+    assert_equal ["h", "e", "l", "l", "o"], reverse.split
   end
 
 end
