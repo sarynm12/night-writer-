96a93a30166d3a6f875fc84933dc22bad3f7a0b3
Saryn Mooney <smooney15@my.whitworth.edu>
2020-08-03 15:08:15 -0600

    added message length test to reverse class

--- test/reverse_test.rb
+++ test/reverse_test.rb
@@ -9,7 +9,7 @@ require './lib/reverse'
 class ReverseTest < Minitest::Test
 
   def test_it_exists
-    characters = "0.0.0.0.0..00.0.0.00\n00.00.0..000.0000..0\n....0.0.0..00.0.0...\n"
+    characters = "0.0.0.0.0..00.0.0.0000.00.0..000.0000..0....0.0.0..00.0.0..."
     reverse = Reverse.new(characters)
     assert_instance_of Reverse, reverse
   end
@@ -18,6 +18,12 @@ class ReverseTest < Minitest::Test
     characters = [["0.", "00", ".."], ["0.", ".0", ".."], ["0.", "0.", "0."], ["0.", "0.", "0."], ["0.", ".0", "0."]]
     reverse = Reverse.new(characters)
     assert_equal "hello", reverse.translate
+  end
+
+  def test_it_can_return_length_of_message
+    characters = "0.0.0.0.0..00.0.0.0000.00.0..000.0000..0....0.0.0..00.0.0..."
+    reverse = Reverse.new(characters)
+    assert_equal 60, reverse.string_length 
   end
 
   def test_it_can_split_into_one_array
