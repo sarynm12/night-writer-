fa336f24b04fcf120b5e4907c27a3fd452f7eb35
Saryn Mooney <smooney15@my.whitworth.edu>
2020-08-03 09:16:46 -0600

    original_message.txt

--- lib/night_reader.rb
+++ lib/night_reader.rb
@@ -1,3 +1,19 @@
 require './lib/reverse'
 require 'pry'
 # ruby ./lib/night_reader.rb braille.txt original_message.txt
+lines = File.open('message.txt', 'r')
+message = File.read(ARGV[0])
+characters_count = message.length
+lines.close
+
+reverse = Reverse.new(message)
+
+english = reverse.braille_to_english
+
+writer = File.open(ARGV[1], 'w')
+
+writer.write(english)
+
+writer.close
+
+puts "Created #{ARGV[1]} containing #{characters_count} characters"
